ac15223f31e3116347c5086d8efa2983b8f65e13
Kaj-Ivar van der Wijst <k.i.vanderwijst@gmail.com>
2020-06-05 14:53:48 +0200

    GEKKO-files removed

--- model/common/emissions.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-from model.common.config import params
-from model.common import data
-
-
-
-def max_emissions(baseline_emissions):
-    emissions_np = np.array([var.value for var in baseline_emissions])
-    return np.sum(emissions_np, axis=0).max()
