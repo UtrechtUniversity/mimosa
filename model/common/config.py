b96203011981c710daa93081ddd8499e60967460
Kaj-Ivar van der Wijst <k.i.vanderwijst@gmail.com>
2021-02-16 17:26:26 +0100

    Changed default config name

--- model/common/config.py
+++ model/common/config.py
@@ -1,6 +1,6 @@
 import yaml
 
-config_filename = 'input/config_IMAGE.yaml'
+config_filename = 'input/config.yaml'
 
 with open(config_filename, 'r', encoding='utf8') as configfile:
     params = yaml.safe_load(configfile)
