a50ca541eeeeb909e89b7e10852165d66a080716
Kaj-Ivar van der Wijst <k.i.vanderwijst@gmail.com>
2023-05-17 15:15:50 +0200

    Updated default run.py

--- run.py
+++ run.py
@@ -3,8 +3,8 @@ from model.mimosa import MIMOSA
 from model.common.config import parseconfig
 
 params = parseconfig.load_params()
+params["emissions"]["carbonbudget"] = False
 
 model1 = MIMOSA(params)
 model1.solve()
-model1.save("run_filename")
-# model1.plot(filename="plot")
+model1.save("run1")
