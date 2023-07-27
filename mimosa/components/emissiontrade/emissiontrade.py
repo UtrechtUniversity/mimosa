cd16f7e296eafac3b298a49a703eb51816e5c23e
Kaj-Ivar van der Wijst <k.i.vanderwijst@gmail.com>
2023-07-27 10:09:02 +0200

    New emission trade, different than global cost pool

--- mimosa/components/emissiontrade/emissiontrade.py
+++ mimosa/components/emissiontrade/emissiontrade.py
@@ -1,20 +1,23 @@
 """
 Model equations and constraints:
 Emission trading module
-Type: trading of emissions, and not costs
+Type: global cost pool
 """
 
 from typing import Sequence
-
 from mimosa.common import (
     AbstractModel,
+    Var,
+    Param,
     GeneralConstraint,
     GlobalConstraint,
     RegionalConstraint,
-    Var,
+    Constraint,
     NonNegativeReals,
     quant,
-    sqrt,
+    RegionalSoftEqualityConstraint,
+    value,
+    soft_min,
 )
 
 from mimosa.components.abatement import AC
@@ -22,7 +25,7 @@ from mimosa.components.abatement import AC
 
 def get_constraints(m: AbstractModel) -> Sequence[GeneralConstraint]:
     """Emission trading equations and constraints
-    (trading of emissions specification)
+    (global cost pool specification)
 
     Necessary variables:
         m.abatement_costs (abatement costs as paid for by this region)
@@ -37,97 +40,81 @@ def get_constraints(m: AbstractModel) -> Sequence[GeneralConstraint]:
     """
     constraints = []
 
-    # Default options for variables defined below
-    opts_emiss = {"units": quant.unit("emissions_unit"), "within": NonNegativeReals}
-    opts_costs = {"units": quant.unit("currency_unit"), "within": NonNegativeReals}
+    m.area_under_MAC = Var(
+        m.t,
+        m.regions,
+        within=NonNegativeReals,
+        initialize=0,
+        units=quant.unit("currency_unit"),
+    )
 
-    # Emission reduction in region r paid for by region r
-    m.own_emission_reduction = Var(m.t, m.regions, **opts_emiss)
-    m.own_emission_reduction_costs = Var(m.t, m.regions, **opts_costs)
+    m.global_carbonprice = Var(m.t)
 
-    # Emission reduction in other regions paid for by region r
-    m.imported_emission_reduction = Var(m.t, m.regions, **opts_emiss)
-    m.imported_emission_reduction_costs = Var(m.t, m.regions, **opts_costs)
-
-    # Emission reduction in region r paid for by other regions
-    m.exported_emission_reduction = Var(m.t, m.regions, initialize=0.01, **opts_emiss)
-    m.exported_emission_reduction_costs = Var(m.t, m.regions, **opts_costs)
-
-    # Actual emission reduction (will be equal to own + exported reductions)
-    m.actual_emission_reduction = Var(m.t, m.regions, **opts_emiss)
-
-    # Totals of imported and exported emissions
-    m.total_exported_reductions = Var(m.t, **opts_emiss)
-    m.total_exported_reduction_costs = Var(m.t, **opts_costs)
-
+    # The global mitigation cost pool:
     constraints.extend(
         [
-            GlobalConstraint(
-                lambda m, t: m.total_exported_reductions[t]
-                == sum(m.exported_emission_reduction[t, r] for r in m.regions),
-                "total_exported_emissions",
+            RegionalConstraint(
+                lambda m, t, r: m.area_under_MAC[t, r]
+                == AC(m.relative_abatement[t, r], m, t, r) * m.baseline[t, r],
+                "abatement_costs",
             ),
+            # Constraint that sets the global carbon price to the average of the regional carbon prices:
             GlobalConstraint(
-                lambda m, t: m.total_exported_reduction_costs[t]
-                == sum(m.exported_emission_reduction_costs[t, r] for r in m.regions),
-                "total_exported_reduction_costs",
-            ),
-            GlobalConstraint(
-                lambda m, t: m.total_exported_reductions[t]
-                == sum(m.imported_emission_reduction[t, r] for r in m.regions),
-                "total_imported_equals_total_exported_emission_reductions",
+                lambda m, t: m.global_carbonprice[t]
+                == sum(
+                    m.carbonprice[t, r] * m.population(m.year(t), r) for r in m.regions
+                )
+                / sum(m.population(m.year(t), r) for r in m.regions),
+                "global_carbonprice",
             ),
         ]
     )
 
+    ## Extra reporting variables:
+
+    m.paid_for_emission_reductions = Var(
+        m.t, m.regions, units=quant.unit("emissionsrate_unit")
+    )
+    m.import_export_emission_reduction_balance = Var(
+        m.t, m.regions, units=quant.unit("emissionsrate_unit")
+    )
+    m.import_export_mitigation_cost_balance = Var(
+        m.t, m.regions, units=quant.unit("currency_unit")
+    )
     constraints.extend(
         [
-            # Costs for imported emission reductions: imported reductions have a global price
-            RegionalConstraint(
-                lambda m, t, r: m.imported_emission_reduction_costs[t, r]
-                == m.imported_emission_reduction[t, r]
-                * m.total_exported_reduction_costs[t]
-                / m.total_exported_reductions[t],
-                "costs_for_imported_emission_reductions",
-            ),
-            # Costs of the own emission reductions: the "cheap" part of the MAC
-            RegionalConstraint(
-                lambda m, t, r: m.own_emission_reduction_costs[t, r]
-                == AC(m.own_emission_reduction[t, r] / m.baseline[t, r], m, t, r)
-                * m.baseline[t, r],
-                "costs_own_emission_reductions",
-            ),
-            # Costs of exported emission reductions: calculated as the "expensive" part of the MAC
-            # This is the area under the MAC for the total emission reduction (own + exported) minus
-            # the costs of the own reductions.
-            RegionalConstraint(
-                lambda m, t, r: m.actual_emission_reduction[t, r]
-                == m.own_emission_reduction[t, r] + m.exported_emission_reduction[t, r],
-                "actual_emission_reduction",
-            ),
-            RegionalConstraint(
-                lambda m, t, r: m.exported_emission_reduction_costs[t, r]
-                == AC(
-                    m.actual_emission_reduction[t, r] / m.baseline[t, r],
-                    m,
-                    t,
-                    r,
-                )
-                * m.baseline[t, r]
-                - m.own_emission_reduction_costs[t, r],
-                "costs_exported_emission_reductions",
-            ),
-            # Actual reductions and actual abatement costs:
-            RegionalConstraint(
-                lambda m, t, r: m.relative_abatement[t, r]
-                == m.actual_emission_reduction[t, r] / m.baseline[t, r],
-                "relative_abatement",
+            GlobalConstraint(
+                lambda m, t: sum(m.abatement_costs[t, r] for r in m.regions)
+                == sum(m.area_under_MAC[t, r] for r in m.regions),
+                "sum_abatement_equals_sum_area_under_mac",
             ),
             RegionalConstraint(
                 lambda m, t, r: m.abatement_costs[t, r]
-                == m.own_emission_reduction_costs[t, r]
-                + 1.02 * m.imported_emission_reduction_costs[t, r],
-                "abatement_costs",
+                == m.area_under_MAC[t, r]
+                + m.import_export_mitigation_cost_balance[t, r]
+                if t > 0
+                else Constraint.Skip,
+                "abatement_costs_equals_area_under_mac_plus_import_export_mitigation_cost_balance",
+            ),
+            # Constraint: from import/export mitigation costs to import/export of emissions using the global carbon price
+            RegionalConstraint(
+                lambda m, t, r: m.import_export_emission_reduction_balance[t, r]
+                == m.import_export_mitigation_cost_balance[t, r]
+                / soft_min(m.global_carbonprice[t])
+                if t > 0
+                else Constraint.Skip,
+                "import_export_emission_reduction_balance",
+            ),
+            # Constraint: paid for emission reductions
+            RegionalConstraint(
+                lambda m, t, r: m.paid_for_emission_reductions[t, r]
+                == (
+                    m.regional_emission_reduction[t, r]
+                    + m.import_export_emission_reduction_balance[t, r]
+                )
+                if t > 0
+                else Constraint.Skip,
+                "paid_for_emission_reductions",
             ),
         ]
     )
