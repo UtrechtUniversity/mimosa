3e836284aa58a646be757d694e85922a213fd6e1
Kaj-Ivar van der Wijst <k.i.vanderwijst@gmail.com>
2023-07-27 17:41:26 +0200

    Updated logo

--- setup.py
+++ setup.py
@@ -3,14 +3,17 @@ from setuptools import setup
 with open("README.md") as fh:
     description = fh.read()
 
+with open("LICENSE") as fh:
+    license_txt = fh.read()
+
 setup(
     name="mimosa",
-    version="0.1.3",
+    version="0.1.4",
     author="Kaj-Ivar van der Wijst",
     author_email="k.i.vanderwijst@gmail.com",
     packages=["mimosa"],
-    # url="http://pypi.python.org/pypi/PackageName/",
-    license="LICENSE.txt",
+    url="https://github.com/kvanderwijst/MIMOSA",
+    license=license_txt,
     description="MIMOSA: Integrated Assessment Model for Cost-Benefit Analysis",
     long_description=description,
     long_description_content_type="text/markdown",
