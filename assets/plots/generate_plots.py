4951c24e5c4be77eacec6796863008304711ed35
Kaj-Ivar van der Wijst <k.i.vanderwijst@gmail.com>
2024-03-11 20:18:37 +0100

    Deployed b4a91fc with MkDocs version: 1.5.3

--- assets/plots/generate_plots.py
+++ assets/plots/generate_plots.py
@@ -130,7 +130,10 @@ fig_baseline_emissions = px.line(
     facet_col_spacing=0.005,
     color="Scenario",
     width=1200,
-).update_layout(
+    render_mode="svg",
+)
+
+fig_baseline_emissions.update_layout(
     legend={"orientation": "h", "y": -1.05, "x": 0.5, "xanchor": "center"},
     margin={"r": 30, "t": 30, "l": 50},
 )
@@ -146,3 +149,182 @@ fig_baseline_emissions.for_each_annotation(
     lambda a: a.update(text=a.text.split("=")[-1])
 )
 fig_baseline_emissions.write_json("docs/assets/plots/baseline_emissions.json")
+
+
+### AR5 TCRE plot
+
+from plotly.subplots import make_subplots
+
+fig_ar5_tcre = make_subplots()
+
+fig_ar5_tcre.add_layout_image(
+    dict(
+        source="../../assets/plots/ar5_wg1_spm10.jpeg",
+        xref="x",
+        yref="y",
+        yanchor="bottom",
+        x=-790,
+        y=-1.01,
+        sizex=10232,
+        sizey=6.677,
+        sizing="stretch",
+        opacity=1,
+        layer="below",
+    )
+)
+
+ar5_annotations = []
+for TCRE, name, xend, xshift in (
+    [0.82, "p95", 6000, -20],
+    [0.62, "p50", 8000, 20],
+    [0.42, "p05", 9100, 40],
+):
+    x = np.linspace(1000, xend)
+    fig_ar5_tcre.add_scatter(
+        x=x,
+        y=TCRE * x / 1000,
+        mode="lines",
+        line={"color": "black", "dash": "dot", "width": 3},
+        name=f"AR5 {name} (TCRE={TCRE} degC/TtCO2)",
+    )
+    ar5_annotations.append(
+        dict(
+            x=5000,
+            y=TCRE * 5000 / 1000,
+            text=f"<b> TCRE={TCRE} </b><br>({name})",
+            xanchor="right" if name == "p95" else "left",
+            font={"size": 12, "color": "black"},
+            showarrow=False,
+            xshift=xshift,
+            bgcolor="white",
+            bordercolor="black",
+        )
+    )
+fig_ar5_tcre.update_layout(annotations=ar5_annotations)
+fig_ar5_tcre.update_xaxes(
+    range=[-790, 10232 - 790], showgrid=False, zeroline=False, visible=False
+).update_yaxes(
+    range=[-1.01, 6.677 - 1.01], visible=False, showgrid=False, zeroline=False
+).update_layout(
+    width=700,
+    height=550,
+    margin={"r": 100, "t": 0, "l": 0, "b": 0},
+    showlegend=False,
+    updatemenus=[
+        dict(
+            type="buttons",
+            buttons=[
+                dict(
+                    label="With TCRE",
+                    method="update",
+                    args=[
+                        {"visible": [True, True, True]},
+                        {"annotations": ar5_annotations},
+                    ],
+                ),
+                dict(
+                    label="Original figure",
+                    method="update",
+                    args=[{"visible": [False, False, False]}, {"annotations": []}],
+                ),
+            ],
+            direction="right",
+            showactive=True,
+            x=0.5,
+            y=1.1,
+            xanchor="center",
+            yanchor="top",
+        )
+    ],
+)
+
+fig_ar5_tcre.write_json("docs/assets/plots/ar5_tcre.json")
+
+
+### AR6 TCRE plot
+fig_ar6_tcre = make_subplots()
+
+ar6_sizex = 6885.2
+ar6_sizey = 5.8888
+ar6_x = -521.8
+ar6_y = -1.993
+
+fig_ar6_tcre.add_layout_image(
+    dict(
+        source="../../assets/plots/ar6_wg1_spm10.png",
+        xref="x",
+        yref="y",
+        yanchor="bottom",
+        x=ar6_x,
+        y=ar6_y,
+        sizex=ar6_sizex,
+        sizey=ar6_sizey,
+        sizing="stretch",
+        opacity=1,
+        layer="below",
+    )
+)
+ar6_annotations = []
+for TCRE, name, yshift, xshift in (
+    [0.75, "p95", 0.2, -20],
+    [0.62, "p50", 0, 20],
+    [0.42, "p05", -0.07, 40],
+):
+    x = np.linspace(2200, 4380)
+    fig_ar6_tcre.add_scatter(
+        x=x,
+        y=(x - 2200) * TCRE / 1000 + 1 + yshift,
+        mode="lines",
+        line={"color": "black", "dash": "dot", "width": 3},
+        name=f"AR6 {name} (TCRE={TCRE} degC/TtCO2)",
+    )
+    ar6_annotations.append(
+        dict(
+            x=4500,
+            y=(4400 - 2200) * TCRE / 1000 + 1 + yshift,
+            text=f"<b> TCRE={TCRE} </b><br>({name})",
+            xanchor="left",
+            font={"size": 12, "color": "black"},
+            showarrow=False,
+            bgcolor="white",
+            bordercolor="black",
+        )
+    )
+fig_ar6_tcre.update_layout(annotations=ar6_annotations)
+fig_ar6_tcre.update_xaxes(
+    range=[ar6_x, 6000], showgrid=False, zeroline=False, visible=False
+).update_yaxes(
+    range=[-0.2, ar6_sizey + ar6_y], visible=False, showgrid=False, zeroline=False
+).update_layout(
+    width=700,
+    height=440,
+    margin={"r": 0, "t": 0, "l": 0, "b": 0},
+    showlegend=False,
+    updatemenus=[
+        dict(
+            type="buttons",
+            buttons=[
+                dict(
+                    label="With TCRE",
+                    method="update",
+                    args=[
+                        {"visible": [True, True, True]},
+                        {"annotations": ar6_annotations},
+                    ],
+                ),
+                dict(
+                    label="Original figure",
+                    method="update",
+                    args=[{"visible": [False, False, False]}, {"annotations": []}],
+                ),
+            ],
+            direction="right",
+            showactive=True,
+            x=0.5,
+            y=1.1,
+            xanchor="center",
+            yanchor="top",
+        )
+    ],
+)
+fig_ar6_tcre.write_json("docs/assets/plots/ar6_tcre.json")
