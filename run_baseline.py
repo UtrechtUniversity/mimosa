c4cbc984cf0894ffa69c40af505bfc7fd5a3f12f
Kaj-Ivar van der Wijst <k.i.vanderwijst@gmail.com>
2024-03-13 09:37:52 +0100

    Update run_baseline.py

--- run_baseline.py
+++ run_baseline.py
@@ -1,20 +1,23 @@
 from mimosa import MIMOSA, load_params
 
 
-for ignore_damages in [False]:
+for ignore_damages in [False, True]:
     params = load_params()
     params["emissions"]["carbonbudget"] = False
-    params["model"]["welfare module"] = "cost_minimising"
-    params["economics"]["MAC"]["gamma"] = "0.00001 USD2005/tCO2"
-    params["simulation"]["simulationmode"] = True
-    params["simulation"]["constraint_variables"] = {
-        "relative_abatement": {
-            year: {region: 0.0 for region in params["regions"]}
-            for year in range(2025, 2151, 5)
-        },
-    }
     params["economics"]["damages"]["ignore damages"] = ignore_damages
-    params["economics"]["damages"]["scale factor"] = 0.0 if ignore_damages else 1.0
+    params["model"]["welfare module"] = "cost_minimising"
+
+    if not ignore_damages:
+        params["simulation"]["simulationmode"] = True
+        params["simulation"]["constraint_variables"] = {
+            "relative_abatement": {
+                year: {region: 0.0 for region in params["regions"]}
+                for year in range(2025, 2151, 5)
+            },
+        }
+        params["economics"]["MAC"]["gamma"] = "0.00001 USD2005/tCO2"
+
+    # Disable
     params["emissions"]["non increasing emissions after 2100"] = False
     params["emissions"]["not positive after budget year"] = False
     params["emissions"]["inertia"]["regional"] = False
@@ -25,5 +28,5 @@ for ignore_damages in [False]:
     model = MIMOSA(params)
     model.solve(verbose=True)
     model.save(
-        f"baseline3_ignore_damages_{ignore_damages}",
+        f"baseline_ignore_damages_{ignore_damages}",
     )
