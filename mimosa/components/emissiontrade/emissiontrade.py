a2a749d71a153f0968991600a9350929476fa32f
Kaj-Ivar van der Wijst <k.i.vanderwijst@gmail.com>
2024-05-01 13:44:18 +0200

    Update emissiontrade.py

--- mimosa/components/emissiontrade/emissiontrade.py
+++ mimosa/components/emissiontrade/emissiontrade.py
@@ -20,12 +20,34 @@ def get_constraints(m: AbstractModel) -> Sequence[GeneralConstraint]:
     In MIMOSA, every region can reduce its own emissions. The price for this is determined
     by the area under the MAC (see [Mitigation](mitigation.md#mitigation-costs)). On top of that,
     regions can trade emission reductions with each other. Regions can pay other regions to reduce
-    their emissions, or receive payments for reducing their own emissions. The financial transfers for
+    their emissions, or receive payments for reducing their own emissions.
+
+    The financial transfers for
     this are captured in the variable $\\text{import/export mitigation cost balance}_{t,r}$. For every timestep,
     the sum of these transfers should be zero:
 
     $$
     \\sum_r \\text{import/export mitigation cost balance}_{t,r} = 0
+    $$
+
+    All emissions are traded at the global carbon price. Therefore, the financial flows (mitigation cost balance) is
+    translated into emission flows (import/export emission reduction balance) using the global carbon price:
+
+    $$
+    \\text{import/export emission reduction balance}_{t,r} = \\frac{\\text{import/export mitigation cost balance}_{t,r}}{\\text{global carbon price}_{t}},
+    $$
+
+    where the global carbon price is the population weighted average of the regional carbon prices:
+
+    $$
+    \\text{global carbon price}_{t} = \\frac{\\sum_r \\text{carbon price}_{t,r} \\cdot \\text{population}_{t,r}}{\\sum_r \\text{population}_{t,r}}.
+    $$
+
+    Finally, the emission reductions paid for by a region are calculated as the physical reductions in the region plus the
+    import/export emission reduction balance:
+
+    $$
+    \\text{paid for emission reductions}_{t,r} = \\text{regional emission reduction}_{t,r} + \\text{import/export emission reduction balance}_{t,r}.
     $$
 
 
