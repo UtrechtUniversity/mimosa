97fa594fad3348d1dd474bd268a916f6e8f6dbee
Kaj-Ivar van der Wijst <k.i.vanderwijst@gmail.com>
2024-05-02 16:02:19 +0200

    MAC tests: check if area under MAC is integral of MAC

--- tests/unittests/test_mitigation.py
+++ tests/unittests/test_mitigation.py
@@ -1,3 +1,6 @@
+import pytest
+import numpy as np
+
 from mimosa.components import mitigation
 
 
@@ -8,11 +11,38 @@ class MockAbstractModel:
         self.learning_factor = [1] * len(self.t)
 
 
-def test_mac():
-    m = MockAbstractModel()
-    m.MAC_scaling_factor = {r: 1 for r in m.regions}
-    m.MAC_gamma = 100
-    m.MAC_beta = 3
+@pytest.fixture
+def m():
+    model = MockAbstractModel()
+    model.MAC_scaling_factor = {r: 1 for r in model.regions}
+    model.MAC_gamma = 100
+    model.MAC_beta = 3
+    return model
 
+
+def test_mac(m):
+    # Zero mitigation should give zero carbon price:
     assert mitigation.MAC(0, m, 0, "r1") == 0
+
+    # Backstop technology (100% reduction) should give the calibration
+    # value gamma as carbon price
     assert mitigation.MAC(1, m, 0, "r1") == m.MAC_gamma
+
+
+@pytest.mark.parametrize("a_end", [0.3, 0.7, 1, 1.5])
+def test_area_under_mac(a_end, request):
+    m = request.getfixturevalue("m")
+
+    # Zero mitigation should give zero abatement costs
+    assert mitigation.AC(0, m, 0, "r1") == 0
+
+    # Test if AC is equal to the integral of MAC:
+    a_values = np.linspace(0, a_end, 500)
+    mac_values = mitigation.MAC(a_values, m, 0, "r1")
+    integral_mac = np.trapz(mac_values, x=a_values)
+
+    abatement_costs = mitigation.AC(a_end, m, 0, "r1")
+
+    # Since integral_mac is a numerical approximation, test if
+    # `integral_mac` is within 0.1% of `abatement_costs`
+    assert integral_mac == pytest.approx(abatement_costs, 0.001 * abatement_costs)
