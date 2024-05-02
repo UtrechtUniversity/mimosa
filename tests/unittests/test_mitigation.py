e559062850c054dc95def8d9fbf88687f5e82752
Kaj-Ivar van der Wijst <k.i.vanderwijst@gmail.com>
2024-05-02 14:23:40 +0200

    First pytest test

---
+++ tests/unittests/test_mitigation.py
@@ -0,0 +1,18 @@
+from mimosa.components import mitigation
+
+
+class MockAbstractModel:
+    def __init__(self):
+        self.t = [0, 1, 2, 3, 4, 5]
+        self.regions = ["r1", "r2", "r3"]
+        self.learning_factor = [1] * len(self.t)
+
+
+def test_mac():
+    m = MockAbstractModel()
+    m.MAC_scaling_factor = [1] * len(m.regions)
+    m.MAC_gamma = 100
+    m.MAC_beta = 3
+
+    assert mitigation.MAC(0, m, 0, "r1") == 0
+    assert mitigation.MAC(1, m, 0, "r1") == m.MAC_gamma
