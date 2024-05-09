ce49da25c614e019245c393358120cd190e8be1e
Kaj-Ivar van der Wijst <k.i.vanderwijst@gmail.com>
2024-05-09 09:09:37 +0200

    Update test_baseline_nodamages.py

--- tests/modeltests/test_baseline_nodamages.py
+++ tests/modeltests/test_baseline_nodamages.py
@@ -1,21 +1,22 @@
 import pytest
-
-
 from tests.modeltests.utils import exec_run, SolverStatus, read_output
+
+pytestmark = pytest.mark.slow
 
 
 @pytest.fixture(scope="module")
 def script_output():
+    """Runs the script shown in the documentation"""
     return exec_run("runs/run_baseline_nodamages.py")
 
 
-@pytest.mark.slow
 def test_run_successfully(script_output):
+    """Run should be successful"""
     assert script_output["model"].status == SolverStatus.ok
 
 
-@pytest.mark.slow
 def test_zero_mitigation_costs(script_output):
+    """Mitigation costs should be zero"""
     model = script_output["model"]
     output_df_ind = read_output(model)
 
@@ -24,8 +25,8 @@ def test_zero_mitigation_costs(script_output):
     ].max().max() == pytest.approx(0.0, abs=0.001)
 
 
-@pytest.mark.slow
 def test_carbonprice_zero(script_output):
+    """Carbon price should be zero"""
     model = script_output["model"]
     output_df_ind = read_output(model)
 
@@ -34,8 +35,8 @@ def test_carbonprice_zero(script_output):
     )
 
 
-@pytest.mark.slow
-def test_emissions_equal_to_baseline(script_output):
+def test_emissions_equal_to_baseline_emissions(script_output):
+    """Regional eissions should be equal to baseline emissions"""
     model = script_output["model"]
     output_df_ind = read_output(model)
 
@@ -44,3 +45,16 @@ def test_emissions_equal_to_baseline(script_output):
 
     diff = (baseline - emissions) / baseline
     assert diff.abs().max().max() < 0.05  # Maximum difference should be less than 5%
+
+
+def test_gdp_equal_to_baseline_gdp(script_output):
+    """GDP should be equal to baseline GDP"""
+    model = script_output["model"]
+    output_df_ind = read_output(model)
+
+    baseline = output_df_ind.loc["baseline_GDP", "2020":]
+    gdp = output_df_ind.loc["GDP_net", "2020":]
+
+    diff = (baseline - gdp) / baseline
+    assert diff.abs().stack().max() < 0.07
+    assert diff.abs().stack().mean() < 0.02
