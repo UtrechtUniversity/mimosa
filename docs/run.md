f42131bf90fbd1490d9b82f6a07d8c2397f33efa
Kaj-Ivar van der Wijst <k.i.vanderwijst@gmail.com>
2024-05-22 09:59:26 +0200

    Effort sharing runs now tested

--- docs/run.md
+++ docs/run.md
@@ -87,27 +87,7 @@ It can be useful to do a MIMOSA run with zero mitigation: a baseline run. We dis
 MIMOSA has some built-in effort sharing regimes. In this example, they are used in combination with a carbon budget (but it could be used in CBA mode). The welfare module is set to cost minimising, as this is typically used with effort sharing regimes. Effort sharing would be impossible without emission trading. Finally, this would often be infeasible for some regions, if we didn't allow for some extra financial transfers beyond just emission trading, which is why we set the relative mitigation cost minimum level to a small negative number.
 
 ```python
-from mimosa import MIMOSA, load_params
-
-
-# Loop over the three available effort sharing regimes
-for regime in [
-     "equal_mitigation_costs",
-     "equal_total_costs",
-     "per_cap_convergence",
-]:
-     params = load_params()
-     params["model"]["emissiontrade module"] = "emissiontrade"
-     params["model"]["welfare module"] = "cost_minimising"
-     params["emissions"]["carbonbudget"] = "700 GtCO2"
-     params["effort sharing"]["regime"] = regime
-     params["economics"]["MAC"]["rel_mitigation_costs_min_level"] = -0.3
-     params["time"]["end"] = 2100
-
-     model1 = MIMOSA(params)
-     model1.solve()
-     model1.save(f"run_{regime}")
-
+--8<-- "tests/runs/run_effortsharing.py"
 ```
 
 ### Advanced: logging
---
+++ tests/modeltests/test_effortsharing.py
@@ -0,0 +1,83 @@
+import pytest
+from tests.modeltests.utils import exec_run, SolverStatus, read_output
+
+pytestmark = pytest.mark.slow
+
+
+@pytest.fixture(scope="module")
+def script_output():
+    """Runs the script shown in the documentation"""
+    return exec_run("runs/run_effortsharing.py")
+
+
+def test_run_successfully(script_output):
+    """Last run should be successful"""
+    assert script_output["model1"].status == SolverStatus.ok
+
+
+def test_equal_mitigation_costs(script_output):
+    """
+    Effort-sharing regime: equal_mitigation_costs
+
+    Check if mitigation costs are equal for all regions
+    """
+    output_df_ind = read_output(filename="run_equal_mitigation_costs")
+
+    mitigation_costs = output_df_ind.loc["rel_mitigation_costs", "2025":]
+
+    relative_standard_deviation = mitigation_costs.std() / mitigation_costs.mean()
+
+    # Check if relative standard deviation is below 1%
+    assert relative_standard_deviation.max() < 0.01
+
+
+def test_equal_total_costs(script_output):
+    """
+    Effort-sharing regime: equal_total_costs
+
+    Check if mitigation plus damage costs (plus optional financial transfers)
+    are equal for all regions
+    """
+    output_df_ind = read_output(filename="run_equal_total_costs")
+
+    total_costs = (
+        output_df_ind.loc["rel_mitigation_costs", "2025":]
+        + output_df_ind.loc["damage_costs", "2025":]
+        + output_df_ind.loc["rel_financial_transfer", "2025":]
+    )
+
+    relative_standard_deviation = total_costs.std() / total_costs.mean()
+
+    # Check if relative standard deviation is below 1%
+    assert relative_standard_deviation.max() < 0.01
+
+
+def test_per_cap_convergence(script_output):
+    """
+    Effort-sharing regime: per_cap_convergence
+
+    Check if emissions per capita are equal after convergence year (2050)
+    Note that the emissions are after emission trade, so they are not equal
+    to the direct variable regional_emissions.
+    """
+    output_df_ind = read_output(filename="run_per_cap_convergence").loc[:, "2020":]
+
+    per_capita_emissions = (
+        output_df_ind.loc["regional_emission_allowances"]
+        / output_df_ind.loc["population"]
+    )
+    per_capita_emissions_deviation = per_capita_emissions.std()
+
+    # Before 2050, emissions per capita are not equal (larger than 0.5 GtCO2 per capita deviation):
+    assert per_capita_emissions_deviation["2030"] > 0.5
+
+    # After 2050, emissions per capita are equal (smaller than 0.05 GtCO2 per capita deviation):
+    assert per_capita_emissions_deviation["2050":].max() < 0.05
+
+    # Check that regional emission allowances are equal to baseline emissions minus paid for emission reductions
+    # (emissions after trade)
+    allowances_diff = output_df_ind.loc["regional_emission_allowances"] - (
+        output_df_ind.loc["baseline"]
+        - output_df_ind.loc["paid_for_emission_reductions"]
+    )
+    assert allowances_diff.max().max() < 0.001
