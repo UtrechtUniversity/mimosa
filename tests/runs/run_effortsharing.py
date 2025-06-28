562ff96109938f2ce5e454ebb97a7c84d283f8fc
Kaj-Ivar van der Wijst <k.i.vanderwijst@gmail.com>
2025-06-28 15:11:07 +0200

    All regimes included in tests

--- tests/runs/run_effortsharing.py
+++ tests/runs/run_effortsharing.py
@@ -6,13 +6,16 @@ for regime in [
     "equal_mitigation_costs",
     "equal_total_costs",
     "per_cap_convergence",
+    "ability_to_pay",
+    "equal_cumulative_per_cap",
 ]:
     params = load_params()
     params["model"]["emissiontrade module"] = "emissiontrade"
     params["model"]["welfare module"] = "cost_minimising"
     params["emissions"]["carbonbudget"] = "700 GtCO2"
+    params["emissions"]["baseline carbon intensity"] = False
     params["effort sharing"]["regime"] = regime
-    params["economics"]["MAC"]["rel_mitigation_costs_min_level"] = -0.3
+    params["economics"]["MAC"]["rel_mitigation_costs_min_level"] = -0.5
     params["time"]["end"] = 2100
 
     model1 = MIMOSA(params)
