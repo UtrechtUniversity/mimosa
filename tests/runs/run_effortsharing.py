740fe31f4ddfc3e35c2399bd55ade72107ef2e7e
Kaj-Ivar van der Wijst <k.i.vanderwijst@gmail.com>
2025-06-28 20:40:33 +0200

    Effort sharing runs updated

--- tests/runs/run_effortsharing.py
+++ tests/runs/run_effortsharing.py
@@ -10,12 +10,12 @@ for regime in [
     "equal_cumulative_per_cap",
 ]:
     params = load_params()
+    params["effort sharing"]["regime"] = regime
     params["model"]["emissiontrade module"] = "emissiontrade"
     params["model"]["welfare module"] = "cost_minimising"
     params["emissions"]["carbonbudget"] = "700 GtCO2"
-    params["emissions"]["baseline carbon intensity"] = False
-    params["effort sharing"]["regime"] = regime
-    params["economics"]["MAC"]["rel_mitigation_costs_min_level"] = -0.5
+    params["emissions"]["baseline carbon intensity"] = False  # (1)!
+    params["economics"]["MAC"]["rel_mitigation_costs_min_level"] = -0.5  # (2)!
     params["time"]["end"] = 2100
 
     model1 = MIMOSA(params)
