e7bf8147575a6b7e90ded0150eb2518b91e4887b
Kaj-Ivar van der Wijst <k.i.vanderwijst@gmail.com>
2026-06-08 14:07:02 +0200

    Split ACCREU damages in different files for each sector

---
+++ mimosa/components/damages/accreu/__init__.py
@@ -0,0 +1,1 @@
+from .all_damages import get_constraints
--- mimosa/components/damages/accreu/sealevelrise.py
+++ mimosa/components/damages/accreu/sealevelrise.py
@@ -1,8 +1,3 @@
-"""
-Model equations and constraints:
-Damage and adaptation costs, ACCREU specification
-"""
-
 from typing import Sequence
 from mimosa.common import (
     AbstractModel,
@@ -19,57 +14,8 @@ from mimosa.common import (
     NonNegativeReals,
 )
 
-from . import coacch
 
-
-def get_constraints(m: AbstractModel) -> Sequence[GeneralConstraint]:
-    """
-    ACCREU damage specification
-
-    Currently, the temperature-dependent damages are taken directly from the COACCH
-    specification.
-
-    """
-    constraints = []
-
-    m.damage_costs = Var(m.t, m.regions, units=quant.unit("fraction_of_GDP"))
-    m.damage_costs_abs = Var(m.t, m.regions, units=quant.unit("currency_unit"))
-    m.damage_scale_factor = Param(doc="::economics.damages.scale factor")
-    m.damage_relative_global = Var(
-        m.t,
-        units=quant.unit("fraction_of_GDP"),
-    )
-    # Total damages are sum of non-SLR and SLR damages
-    constraints.extend(
-        [
-            RegionalEquation(
-                m.damage_costs,
-                lambda m, t, r: m.damage_costs_non_slr[t, r] + m.damage_costs_slr[t, r],
-            ),
-            RegionalEquation(
-                m.damage_costs_abs,
-                lambda m, t, r: m.damage_costs[t, r] * m.GDP_gross[t, r],
-            ),
-            GlobalEquation(
-                m.damage_relative_global,
-                lambda m, t: (
-                    sum(m.damage_costs_abs[t, r] for r in m.regions)
-                    / m.global_GDP_gross[t]
-                ),
-            ),
-        ]
-    )
-
-    # Get constraints for temperature dependent damages
-    constraints.extend(coacch.get_constraints_temperature_dependent(m))
-
-    # Get constraints for sea-level rise damages
-    constraints.extend(get_constraints_slr(m))
-
-    return constraints
-
-
-def get_constraints_slr(m):
+def get_constraints(m):
     """
     Defines the equations for the gross damages, residual damages, and
     adaptation costs due to sea-level rise (SLR).
