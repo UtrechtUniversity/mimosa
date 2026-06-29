9ae39b5eaa353733865d496659e9ee7396cafe8f
matthiassaihttam <154236498+matthiassaihttam@users.noreply.github.com>
2026-06-29 17:30:42 +0200

    trying to do runs

--- run.py
+++ run.py
@@ -4,8 +4,9 @@ from mimosa import MIMOSA, load_params
 
 params = load_params()
 
-params["model"]["welfare module"] = "welfare_loss_minimising_quintiles"
+params["model"]["welfare module"] = "welfare_loss_minimising"
+params["model"]["damage module"] = "COACCH"
 
 model1 = MIMOSA(params)
 model1.solve()
-model1.save("run_inequality_1_test_units")
+model1.save("20260629_-_welfare_loss_minimising")
