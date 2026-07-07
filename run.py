35e551c17fa6792e310fa86ace8084ef39c71cbb
Kaj-Ivar van der Wijst <k.i.vanderwijst@gmail.com>
2026-07-07 22:16:21 +0200

    Revert to original run.py

--- run.py
+++ run.py
@@ -2,13 +2,7 @@ from mimosa import MIMOSA, load_params
 
 params = load_params()
 
-params["economics"]["damages"]["ignore damages"] = False
-params["time"]["end"] = 2150
-params["model"]["damage module"] = "ACCREU"
+model1 = MIMOSA(params)
+model1.solve()
 
-model = MIMOSA(params)
-
-model.simulator.plot_dependency_graph()
-simulation = model.run_nopolicy_baseline()
-
-model.save_simulation(simulation, "baseline_nopolicy_accreu_newslr")
+model1.save("run1")
