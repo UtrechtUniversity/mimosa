e3019db72cf436ac10e140201f65b49b160e33da
Kaj-Ivar van der Wijst <k.i.vanderwijst@gmail.com>
2026-08-18 17:30:08 +0200

    Deployed 649ddc7 to 1.4.0 with MkDocs 1.6.1 and mike 2.2.0

---
+++ 1.4.0/workshops/summerschool_prisma_2025/run_effortsharing.py
@@ -0,0 +1,17 @@
+from mimosa import MIMOSA, load_params
+
+regime = "PUT_YOUR_EFFORT_SHARING_REGIME_HERE"  # (1)!
+
+params = load_params()
+params["model structure"]["effortsharing module"] = regime
+
+params["model structure"]["emissiontrade module"] = "emissiontrade"
+params["model structure"]["welfare module"] = "cost_minimising"
+params["emissions"]["carbonbudget"] = "700 GtCO2"
+params["emissions"]["baseline carbon intensity"] = False
+params["economics"]["MAC"]["rel_mitigation_costs_min_level"] = -0.5
+params["time"]["end"] = 2100
+
+model3 = MIMOSA(params)
+model3.solve()
+model3.save(f"run_{regime}")
