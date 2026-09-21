323cecd6f65bc1d3bb58249eec31479664a49fb1
Kaj-Ivar van der Wijst <k.i.vanderwijst@gmail.com>
2026-09-21 14:38:44 +0200

    Revert wrong change in inertia calculation

--- mimosa/components/emissions.py
+++ mimosa/components/emissions.py
@@ -584,7 +584,12 @@ def _get_inertia_and_budget_constraints(
                     >= m.period_length[t]
                     * m.inertia_regional
                     * m.ssp_baseline_emissions[0, r]
-                    if value(m.inertia_regional) is not False and t > 0
+                    if value(m.inertia_regional) is not False
+                    and t > 0
+                    and (
+                        value(m.delay_mitigation_year) is False
+                        or m.year(t) >= m.delay_mitigation_year
+                    )
                     else Constraint.Skip
                 ),
                 name="regional_inertia",
